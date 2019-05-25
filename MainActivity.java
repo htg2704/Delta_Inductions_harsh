ea2d69dff392b62d23dc4209d89b105f1a22b047
htg2704 <49567662+htg2704@users.noreply.github.com>
2019-05-25 15:20:31 +0530

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -2,20 +2,24 @@ package com.example.helpdeath2;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.icu.text.UnicodeSetSpanner;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
+import java.lang.Math;
 
 public class MainActivity extends AppCompatActivity {
     EditText correct, guess;
     TextView result;
     Button submit, check;
-    int x = 0;
+    LinearLayout bg;
+    int x = 0, c = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,7 +30,7 @@ public class MainActivity extends AppCompatActivity {
         result = (TextView) findViewById(R.id.result);
         submit = (Button) findViewById(R.id.button);
         check = (Button) findViewById(R.id.button2);
-
+        bg = (LinearLayout) findViewById(R.id.back);
         submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -69,6 +73,8 @@ public class MainActivity extends AppCompatActivity {
 
     public void compare(int ca, int ga){
         String resultlabel = "";
+        SharedPreferences sh = getPreferences(Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sh.edit();
         if(ga>ca){
             resultlabel = "Your guess was wrong and it was more than correct age";
             result.setText(resultlabel);
@@ -86,13 +92,15 @@ public class MainActivity extends AppCompatActivity {
             resultlabel = "Your guess was correct";
             result.setText(resultlabel);
             guess.setText("");
+            c++;
+            editor.putInt(getString(R.string.app_name), c);
+
 
         }
 
         if(ga!=ca){
             x++;
-            SharedPreferences sh = getPreferences(Context.MODE_PRIVATE);
-            SharedPreferences.Editor editor = sh.edit();
+
             editor.putInt(getString(R.string.app_name), x);
             editor.apply();
             int y = sh.getInt(getString(R.string.app_name), x);
@@ -103,7 +111,25 @@ public class MainActivity extends AppCompatActivity {
                 correct.setText("");
                 result.setText("Result will be shown here");
             }
+        int d;
+            if((ga-ca)>=0)
+                d = ga - ca;
+            else
+                d = ca - ga;
+
+        if(d>15)
+        {
+            bg.setBackgroundColor(Color.RED);
         }
+            if(d<5)
+            {
+                bg.setBackgroundColor(Color.GREEN);
+            }
+         if((d>5&&d<15)||d==0)
+             bg.setBackgroundColor(Color.WHITE);
+        }
+
+        Toast.makeText(getApplicationContext(),"Failed: "+ x + " times\n" + "Guessed Correctly: "+ c + " times",Toast.LENGTH_LONG).show();
     }
 
 }
